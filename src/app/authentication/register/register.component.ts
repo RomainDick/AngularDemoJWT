858f9c38d5e2359553b287f3b18347207ea3b700
Unknown <dck.romain@gmail.com>
2019-10-29 17:00:06 +0100

    feat(register) : add form validation

--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthenticationService } from '../services';
 import { first } from 'rxjs/operators';
@@ -12,11 +12,9 @@ import { first } from 'rxjs/operators';
 export class RegisterComponent implements OnInit {
 
 	registerForm: FormGroup;
-	submitted = false;
 	error = '';
 
 	constructor(
-		private formBuilder: FormBuilder,
 		private route: ActivatedRoute,
 		private router: Router,
 		private authenticationService: AuthenticationService
@@ -28,33 +26,32 @@ export class RegisterComponent implements OnInit {
 	}
 
 	ngOnInit() {
-		this.registerForm = this.formBuilder.group({
-			username: ['', Validators.required],
-			mail: ['', Validators.required, Validators.email],
-			password: ['', Validators.required],
-			repeatPassword: ['', Validators.required],
+		this.registerForm = new FormGroup({
+			username: new FormControl('', Validators.required),
+			mail: new FormControl('', [Validators.required, Validators.email]),
+			password: new FormControl('', Validators.required),
+			repeatPassword: new FormControl('', Validators.required),
 		});
+	}
+
+	hasError(controlName: string, errorName: string) {
+		return this.registerForm.controls[controlName].hasError(errorName);
 	}
 
 	// convenience getter for easy access to form fields
 	get f() { return this.registerForm.controls; }
 
 	onSubmit() {
-	    this.submitted = true;
-
-	    // stop here if form is invalid
-	    if (this.registerForm.invalid) {
-	        return;
-	    }
-
-	    this.authenticationService.register(this.f.username.value, this.f.mail.value, this.f.password.value)
-	        .pipe(first())
-	        .subscribe(
-	            data => {
-	                this.router.navigate(["/"]);
-	            },
-	            error => {
-	                this.error = error;
-	            });
+		if (this.registerForm.valid) {
+			this.authenticationService.register(this.f.username.value, this.f.mail.value, this.f.password.value)
+				.pipe(first())
+				.subscribe(
+					data => {
+						this.router.navigate(["/"]);
+					},
+					error => {
+						this.error = error;
+					});
+		}
 	}
 }
